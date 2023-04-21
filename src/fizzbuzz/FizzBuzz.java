e71be814eb4be23163eb556ef1458c9aa4c44c34
Nathan ROSET <nathroset97@gmail.com>
2023-04-21 14:24:58 +0200

    gros refactor test passe

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,17 +1,15 @@
 package fizzbuzz;
 
 public class FizzBuzz {
-	
-	public static final String UN = "1";
-	public static final String DEUX = "2";
-	public static final String FIZZ = "fizz";
-	public static final String BUZZ = "buzz";
+
+	private int[] nbSpeciaux = {3,5};
+	private String[] valeursSpeciales = {"fizz","buzz"};
 	
 	public String fizzBuzz(int input) {
-		if (input == 3)
-			return FIZZ;
-		if (input == 5)
-			return BUZZ;
+		for (int i = 0; i < nbSpeciaux.length ; i++) {
+			if (input == nbSpeciaux[i])
+				return valeursSpeciales[i];
+		}
 		return Integer.toString(input);
 	}
 }
